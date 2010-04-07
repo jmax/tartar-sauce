664344ccb048b046b240c9fd4d011bc9ecc18214
Juan María Martínez Arce <juan@insignia4u.com>
2010-04-07 17:32:11 -0300

    Add .gems file for heroku deployment.

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ Rails::Initializer.run do |config|
 
   config.gem "friendly_id",         :version => '~> 2.3'
 
-  config.gem "authlogic"
+  config.gem "authlogic",           :version => '2.1.2'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
